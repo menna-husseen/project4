c417d12b88f17f4ed5d8d872a100e87b8fc805b2
menna-husseen <119748650+menna-husseen@users.noreply.github.com>
2023-07-28 00:42:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+// window.alert('hello js')
+document.getElementById('demo').innerHTML="hello"
+console.log('ahmed ali')
+
+var userName ="Ahmed";
+var userAge=25;
+console.log("hello "+userName+" yourAgeIs "+userAge)
